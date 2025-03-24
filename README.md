b5961e17b101cbb3de63046f62bb390d1e50a196
Xefreh <william.strainchamps@gmail.com>
2025-03-24 09:48:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Chifoumi
+
+Chifoumi est un site internet où vous pouvez jouer à Pierre-Feuille-Ciseaux contre un bot.
+Il est possible de visualiser le site internet à partir de cette url: https://xefreh.github.io/chifoumi/.
---
+++ main.js
@@ -0,0 +1,70 @@
+const pierre_button = document.querySelector("#pierre");
+const feuille_button = document.querySelector("#feuille");
+const ciseaux_button = document.querySelector("#ciseaux");
+
+const reset_button = document.querySelector("#reset");
+
+const resultats = document.querySelector("#resultats");
+
+const regles_du_jeu = {
+  pierre: "ciseaux",
+  feuille: "pierre",
+  ciseaux: "feuille",
+};
+
+let choix_utilisateur = "";
+let choix_ordinateur = "";
+let resultat = "";
+
+let victoires = 0;
+let defaites = 0;
+let egalites = 0;
+
+function jouer(event) {
+  choix_utilisateur = event.target.id;
+
+  const choix = Object.keys(regles_du_jeu);
+  const randomIndex = Math.floor(Math.random() * choix.length);
+  choix_ordinateur = choix[randomIndex];
+
+  if (choix_utilisateur === choix_ordinateur) {
+    resultat = "Egalité";
+    egalites++;
+  } else if (regles_du_jeu[choix_utilisateur] === choix_ordinateur) {
+    resultat = "Victoire";
+    victoires++;
+  } else {
+    resultat = "Défaite";
+    defaites++;
+  }
+
+  mettre_a_jour_score();
+}
+
+function mettre_a_jour_score() {
+  resultats.innerHTML = `
+    <p>Votre choix : ${choix_utilisateur}</p>
+    <p>Choix de l'ordinateur : ${choix_ordinateur}</p>
+    <p>Résultat : ${resultat}</p>
+    
+    <p>Victoires : ${victoires}</p>
+    <p>Défaites : ${defaites}</p>
+    <p>Égalités : ${egalites}</p>
+  `;
+}
+
+function reset() {
+  choix_utilisateur = "";
+  choix_ordinateur = "";
+  resultat = "";
+  victoires = 0;
+  defaites = 0;
+  egalites = 0;
+  resultats.innerHTML = "";
+}
+
+pierre_button.addEventListener("click", jouer);
+feuille_button.addEventListener("click", jouer);
+ciseaux_button.addEventListener("click", jouer);
+
+reset_button.addEventListener("click", reset);
